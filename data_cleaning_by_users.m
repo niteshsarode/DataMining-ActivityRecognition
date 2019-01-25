e080e87f94aaed78f8f6382420188b4dc0a081f5
Nitesh Sarode <sarodenitesh.89@gmail.com>
2019-01-25 00:01:37 -0700

    Code Cleaning

--- data_cleaning_by_users.m
+++ data_cleaning_by_users.m
@@ -7,58 +7,35 @@ for j=3:4
          files = dir("MyoData/"+folders_myo(j).name+"/fork/*.*");
          files_gt = dir("groundTruth/user9/fork/*.txt");
          for k=1:length(files)
-              if contains(files(k).name,"EMG")
-                  emg_e_mat = {};
-                  FileNames = files(k).name;
-                  f_data = csvread("MyoData/"+folders_myo(j).name+"/fork/"+files(k).name);
-                  tf_data = csvread("groundTruth/user9/fork/"+files_gt(1).name);
-                  start_sample = {};
-                  end_sample = {};
-                  for l=1:length(tf_data)
-                      start_sample = [start_sample;(round(tf_data(l,1)/30,3)*50)];
-                      end_sample = [end_sample;(round(tf_data(l,2)/30,3)*50)];
-                  end
-                  s_t = cell2mat(start_sample);
-                  e_t = cell2mat(end_sample);
-                  cursor = 1;
-                  for l=1:length(tf_data)
-                      for t=cursor:e_t(l)
-                          if t < s_t(l)
-                              emg_e_mat = [emg_e_mat;f_data(t,:),0]; % 0 for non-eating
-                          elseif t >= s_t(l) && t <= e_t(l)
-                              emg_e_mat = [emg_e_mat;f_data(t,:),1]; % 1 for eating
-                          end
-                      end
-                      cursor = floor(e_t(l));
-                  end
-                  disp(emg_e_mat)
-              end
-              if contains(files(k).name,"IMU")
-                  imu_e_mat = {};
-                  FileNames = files(k).name;
-                  f_data = csvread("MyoData/"+folders_myo(j).name+"/fork/"+files(k).name);
-                  tf_data = csvread("groundTruth/user9/fork/"+files_gt(1).name);
-                  start_sample = {};
-                  end_sample = {};
-                  for l=1:length(tf_data)
-                      start_sample = [start_sample;(round(tf_data(l,1)/30,3)*50)];
-                      end_sample = [end_sample;(round(tf_data(l,2)/30,3)*50)];
-                  end
-                  s_t = cell2mat(start_sample);
-                  e_t = cell2mat(end_sample);
-                  cursor = 1;
-                  for l=1:length(tf_data)
-                      for t=cursor:e_t(l)
-                          if t < s_t(l)
-                              imu_e_mat = [imu_e_mat;f_data(t,:),0]; % 0 for non-eating
-                          elseif t >= s_t(l) && t <= e_t(l)
-                              imu_e_mat = [imu_e_mat;f_data(t,:),1]; % 1 for eating
-                          end
-                      end
-                      cursor = floor(e_t(l));
-                  end
-                  disp(imu_e_mat)
+             if contains(files(k).name,"EMG") || contains(files(k).name,"IMU")
+                  calc(files(k).name,files_gt(1).name)
               end
          end
      end
 end
+
+function calc(file_myo,file_gt)
+    disp(file_myo)
+    e_mat = [];
+    f_data = csvread("MyoData/user09/fork/"+file_myo);
+    tf_data = csvread("groundTruth/user9/fork/"+file_gt);
+    s_t = [];
+    e_t = [];
+    for l=1:length(tf_data)
+      s_t = [s_t;(round(tf_data(l,1)/30,3)*50)];
+      e_t = [e_t;(round(tf_data(l,2)/30,3)*50)];
+    end
+    cursor = 1;
+    for l=1:length(tf_data)
+      for t=cursor:e_t(l)
+          if t < s_t(l)
+              e_mat = [e_mat;f_data(t,:),0]; % 0 for non-eating
+          elseif t >= s_t(l) && t <= e_t(l)
+              e_mat = [e_mat;f_data(t,:),1]; % 1 for eating
+          end
+      end
+      cursor = floor(e_t(l));
+    end
+    disp(e_mat)
+end                  
+
