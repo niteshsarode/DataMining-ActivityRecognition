499bdda533280bf7d98022bdaf95118e3afed579
piyush <piyushdoke24@gmail.com>
2019-01-29 23:24:37 -0700

    Added rms, mean and sum

--- dm.m
+++ dm.m
@@ -1,61 +1,125 @@
-format long;
-
+format shortG;
 folders_myo = dir("MyoData/");
 folders_gT = dir("groundTruth/");
-e_mat = {};
-ne_mat = {};
-for j=3:length(folders_myo)
+
+for j=3:3
     disp(folders_myo(j).name)
-      if contains(folders_myo(j).name,"user")
-          files = dir("MyoData/"+folders_myo(j).name+"/fork/*.*");
-          files_gt = dir("groundTruth/"+folders_gT(j).name+"/fork/*.*");
-          for k=1:length(files)
-               if contains(files(k).name,"EMG")
-                   FileNames = files(k).name;
-                   f_data = csvread("MyoData/"+folders_myo(j).name+"/fork/"+files(k).name);
-                   tf_data = csvread("groundTruth/"+folders_gT(j).name+"/fork/"+files_gt(k).name);
-                   start_time = {};
-                   end_time = {};
-                   for l=1:length(tf_data)
-                       start_time = [start_time;(round(tf_data(l,1)/30,3)*1000)];
-                       end_time = [end_time;(round(tf_data(l,2)/30,3)*1000)];
-                   end
-                   first_sample = f_data(1,1);
-                   s_t = cell2mat(start_time);
-                   e_t = cell2mat(end_time);
-                   cursor = 1;
-                   for l=1:length(tf_data)
-                       s = first_sample + s_t(l);
-                       e = first_sample + e_t(l);
-                       for t=cursor:length(f_data)
-                           if f_data(t,1) >= s && f_data(t,1) <= e
-                               e_mat = [e_mat;f_data(t,:)];  
-                               cursor = t;
-                           end
-                           if f_data(t,1) < s
-                               ne_mat = [ne_mat;f_data(t,:)];
-                           end
-                           if f_data(t,1) > e
-                               break
-                           end
-                       end
-                   end
-               end
-          end
-      end
+    if contains(folders_myo(j).name,"user")
+         files = dir("MyoData/"+folders_myo(j).name+"/fork/*.*");
+         files_gt = dir("groundTruth/user9/fork/*.txt");
+         for k=1:length(files)
+             if contains(files(k).name,"EMG")
+                  calc(files(k).name,files_gt(1).name)
+              end
+         end
+     end
 end
 
-%  e_mat conatins eating samples and ne_mat conatins non-eating samples.
+function calc(file_myo,file_gt)
+    disp(file_myo)
+    x_ticks = [];
+    e_mat = [];
+    f_data = csvread("MyoData/user09/fork/"+file_myo);
+    tf_data = csvread("groundTruth/user9/fork/"+file_gt);
+    s_t = [];
+    e_t = [];
+    for l=1:length(tf_data)
+      s_t = [s_t;(round(tf_data(l,1)/30,3)*50)];
+      e_t = [e_t;(round(tf_data(l,2)/30,3)*50)];
+    end
+    cursor = 1;
+    flag = f_data(1,end);
+%     disp(flag)
+    for l=1:length(tf_data)
+      for t=cursor:e_t(l)
+          if t < s_t(l)
+              e_mat = [e_mat;f_data(t,:),0]; % 0 for non-eating
+          elseif t >= s_t(l) && t <= e_t(l)
+              e_mat = [e_mat;f_data(t,:),1]; % 1 for eating
+          end
+          if(f_data(t,end) ~= flag)
+              x_ticks = [x_ticks f_data(t, 1)];
+              flag = f_data(t,end);
+          end
+      end
+      cursor = floor(e_t(l));
+    end
+    
+    %fft function call
+    calcFFT(e_mat(:,2:9))
+    
+    %Mean function for the matrix
+    f_mean(e_mat(:, 2:9));
+    
+    %sum function for the matrix
+    f_sum(e_mat(:, 2:9));
+    
+    %RMS along the column of the matrix
+    f_rms(e_mat(:, 2:9));
+end            
 
-mat = cell2mat(e_mat);
-disp(class(mat));
+function calcFFT(mat)
+    X = fftshift(fft(mat(1:50, :)));
+    pxx = pwelch(X);
+    plot(pxx)
+end
 
-mat(:, 1) = []; %deleted the timestamp from the matrix
+function f_sum(mat)
+    flag = mat(1, end);
+    temp_arr = [];
+    final_mat = [];
+    for l = 1:length(mat(:,1))
+        if(mat(l, end) ~= flag)
+            %function used
+            temp_arr = sum(temp_arr, 1);
+            flag = mat(l, end);
+            final_mat = [final_mat; temp_arr];
+            temp_arr = [];
+        else
+            temp_arr = [temp_arr ; mat(l, :)];
+        end
+    end
+    disp(length(final_mat(:, 1)));
+    plot(final_mat(1:50, :));
+    xticks(0:1:816);
+end
 
-%Fourier transform
-y = fft(mat, [], 2);
-% power spectral density of the FFT
-pxx = pwelch(y);
-plot(pxx)
-ylim([0 500])
-legend('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h') %for 8 attributes  EMG 1, EMG 2, EMG 3, EMG 4, EMG 5, EMG 6, EMG 7, and EMG 8.
+function f_mean(mat)
+    flag = mat(1, end);
+    temp_arr = [];
+    final_mat = [];
+    for l = 1:length(mat(:,1))
+        if(mat(l, end) ~= flag)
+            %function used
+            temp_arr = mean(temp_arr, 1);
+            flag = mat(l, end);
+            final_mat = [final_mat; temp_arr];
+            temp_arr = [];
+        else
+            temp_arr = [temp_arr ; mat(l, :)];
+        end
+    end
+    disp(length(final_mat(:, 1)));
+    plot(final_mat(1:50, :));
+    xticks(0:1:816);
+end
+
+function f_rms(mat)
+    flag = mat(1, end);
+    temp_arr = [];
+    final_mat = [];
+    for l = 1:length(mat(:,1))
+        if(mat(l, end) ~= flag)
+            %function used
+            temp_arr = rms(temp_arr, 1);
+            flag = mat(l, end);
+            final_mat = [final_mat; temp_arr];
+            temp_arr = [];
+        else
+            temp_arr = [temp_arr ; mat(l, :)];
+        end
+    end
+    disp(length(final_mat(:, 1)));
+    plot(final_mat(1:25, :));
+    xticks(0:1:816);
+end
