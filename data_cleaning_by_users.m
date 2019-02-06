c5231d55a1df52255cd754ae2717e6486942d651
Nitesh Sarode <sarodenitesh.89@gmail.com>
2019-02-05 21:48:40 -0700

    PCA done

--- data_cleaning_by_users.m
+++ data_cleaning_by_users.m
@@ -1,24 +1,26 @@
 format shortG;
 folders_myo = dir("MyoData/");
 folders_gT = dir("groundTruth/");
-for j=3:4
-    disp(folders_myo(j).name)
-    if contains(folders_myo(j).name,"user")
-         files = dir("MyoData/"+folders_myo(j).name+"/fork/*.*");
-         files_gt = dir("groundTruth/user9/fork/*.txt");
-         for k=1:length(files)
-             if contains(files(k).name,"EMG")  % || contains(files(k).name,"IMU")
-                  calc(files(k).name,files_gt(1).name)
-              end
-         end
+% for j=3:4
+%     disp(folders_myo(j).name)
+%     if contains(folders_myo(j).name,"user")
+ files = dir("MyoData/user40/fork/*.*");
+ files_gt = dir("groundTruth/user40/fork/*.txt");
+ for k=1:length(files)
+     if contains(files(k).name,"IMU")
+          calc(files(k).name,files_gt(1).name,"IMU")
+     elseif contains(files(k).name,"EMG")
+          calc(files(k).name,files_gt(1).name,"EMG")
      end
-end
+ end
+%      end
+% end
 
-function calc(file_myo,file_gt)
+function calc(file_myo,file_gt,sensor)
     disp(file_myo)
     e_mat = [];
-    f_data = csvread("MyoData/user09/fork/"+file_myo);
-    tf_data = csvread("groundTruth/user9/fork/"+file_gt);
+    f_data = csvread("MyoData/user40/fork/"+file_myo);
+    tf_data = csvread("groundTruth/user40/fork/"+file_gt);
     s_t = [];
     e_t = [];
     for l=1:length(tf_data)
@@ -36,12 +38,65 @@ function calc(file_myo,file_gt)
       end
       cursor = floor(e_t(l));
     end
-    calcFFT(e_mat(:,2:9))
-end                  
-
-function calcFFT(mat)
-    disp(length(mat))
-    X = fftshift(fft(mat));
-    pxx = pwelch(X);
-    plot(pxx)
+    
+    [sep_e_mat, sep_non_e_mat] = separate_classes(e_mat(:,2:10));
+    [e_mat_ext, non_e_mat_ext] = separate_extracted_features(sep_e_mat, sep_non_e_mat, sensor);
+    calc_pca(e_mat_ext,sensor);
+    calc_pca(non_e_mat_ext,sensor);
+    
 end
+
+function [sep_e_mat, sep_non_e_mat] = separate_classes(mat)
+    disp(size(mat))
+    sep_e_mat = [];
+    sep_non_e_mat = [];
+    
+     for l = 1:size(mat)
+         if mat(l,9) == 1
+             sep_e_mat = [sep_e_mat;mat(l,1:8)];
+         else
+             sep_non_e_mat = [sep_non_e_mat;mat(l,1:8)];
+         end
+     end
+
+    disp(size(sep_e_mat));
+    disp(size(sep_non_e_mat));
+end
+
+function [e_mat_ext, non_e_mat_ext] = separate_extracted_features(sep_e_mat, sep_non_e_mat, sensor)
+    if sensor == "EMG"
+        disp(sensor)
+        e_mat_ext = sep_e_mat(:,[1 3 5 6]);
+        non_e_mat_ext = sep_non_e_mat(:,[1 3 5 6]);
+    end
+    if sensor == "IMU"
+        disp(sensor)
+        e_mat_ext = sep_e_mat(:,[1 2 6 7 8]);
+        non_e_mat_ext = sep_non_e_mat(:,[1 2 6 7 8]);
+    end
+end
+
+function calc_pca(mat, sensor)
+    if sensor == "IMU"
+        numberOfPCAComponents = 5
+    elseif sensor == "EMG"
+        numberOfPCAComponents = 4
+    end
+    [coeff,score] = pca(mat);
+    pcaFeatureMatrix = mat * coeff;
+    [rows,col] = size(pcaFeatureMatrix);
+    for n=1:rows
+        plot(pcaFeatureMatrix(n,1:numberOfPCAComponents));
+        hold on;
+    end
+    title(sensor);
+    figure();
+    xlabel('PCA Components');
+    ylabel('Values for the PCA Components');
+    hold off
+    biplot(coeff(:,1:3),'scores',score(:,1:3));
+    title(sensor);
+    figure();
+end
+
+
